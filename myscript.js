22072bb8250a7f3b52538defa1358177eae4d4ff
QFKS <111087976+QFKS@users.noreply.github.com>
2022-10-05 14:50:55 +0300

    11

---
+++ myscript.js
@@ -0,0 +1,30 @@
+//number
+
+let x = prompt('number value1');
+let y = prompt('number value2');
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x+y+y);
+console.log((x/y)+x);
+
+//string
+
+let firstVar = prompt('User name', 'name');
+console.log(firstVar);
+document.writeln(firstVar);
+
+let secondVar = prompt('User last name', 'last name');
+console.log(secondVar);
+document.write(secondVar);
+ 
+//boolean
+
+
+let uk = true;
+let ru = false;
+
+document.write();
+
